4d9a934c0de1421fa641c81c032f2598b87ed9a1
Jelle Schut <jelleschut@hotmail.com>
2021-03-26 13:12:10 +0100

    scripts

---
+++ scripts/StartBackend.ps1
@@ -0,0 +1,12 @@
+﻿if($psISE) {
+    $scriptDir = Split-Path -Path $psISE.CurrentFile.FullPath
+}
+else 
+{
+    $scriptDir = $Global:PSScriptRoot
+}
+
+cd $scriptDir
+
+dotnet run -p ..\Backend\Backend\EindCase.Api\EindCase.Api.csproj
+pause
---
+++ scripts/StartFrontend.ps1
@@ -0,0 +1,16 @@
+﻿if($psISE) {
+    $scriptDir = Split-Path -Path $psISE.CurrentFile.FullPath
+}
+else 
+{
+    $scriptDir = $Global:PSScriptRoot
+}
+
+npm install -g @angular/cli
+
+cd $scriptDir
+cd ..\Frontend\frontend
+
+ng serve
+
+pause
